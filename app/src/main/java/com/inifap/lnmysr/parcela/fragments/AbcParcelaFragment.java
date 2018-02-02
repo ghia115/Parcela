c348a4e5bfd54c422b980df2f86c2568a6a92ec2
ghia115 <casaghia115@gmail.com>
2018-02-02 15:59:15 -0600

    toolbar

--- app/src/main/java/com/inifap/lnmysr/parcela/fragments/AbcParcelaFragment.java
+++ app/src/main/java/com/inifap/lnmysr/parcela/fragments/AbcParcelaFragment.java
@@ -1,46 +1,32 @@
 package com.inifap.lnmysr.parcela.fragments;
 
 
-import android.Manifest;
-import android.app.AlertDialog;
 import android.content.ContentValues;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.database.sqlite.SQLiteDatabase;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
 import com.inifap.lnmysr.parcela.R;
 import com.inifap.lnmysr.parcela.helperDataBase.BBDD_Helper;
 import com.inifap.lnmysr.parcela.helperDataBase.Estructura_BBDD;
-
-import static android.content.Context.LOCATION_SERVICE;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AbcParcelaFragment extends Fragment {
 
-    AlertDialog alert = null;
-    LocationManager locationManager;
-    LocationListener locationListener;
     private EditText parcela, latitud, longitud;
     FloatingActionButton ubicacion;
     Button updateBase;
@@ -55,12 +41,13 @@ public class AbcParcelaFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_abc_parcela, container, false);
+        showToolbar(getResources().getString(R.string.toolbar_tittle_abc), false);
 
-        parcela = (EditText) rootView.findViewById(R.id.parcela);
-        latitud = (EditText) rootView.findViewById(R.id.latitud);
-        longitud = (EditText) rootView.findViewById(R.id.longitud);
-        ubicacion = (FloatingActionButton) rootView.findViewById(R.id.ubicacion);
-        updateBase = (Button) rootView.findViewById(R.id.updateBase);
+        parcela = rootView.findViewById(R.id.parcela);
+        latitud = rootView.findViewById(R.id.latitud);
+        longitud = rootView.findViewById(R.id.longitud);
+        ubicacion = rootView.findViewById(R.id.ubicacion);
+        updateBase = rootView.findViewById(R.id.updateBase);
 
         updateBase.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -87,39 +74,6 @@ public class AbcParcelaFragment extends Fragment {
             }
         });
 
-        locationManager = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
-        locationListener = (LocationListener) new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                latitud.append("\n" + location.getLatitude() + " " + location.getLongitude());
-            }
-
-            @Override
-            public void onStatusChanged(String s, int i, Bundle bundle) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String s) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String s) {
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(intent);
-            }
-        };
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
-                        ,10);
-            }
-            return null;
-        }else {
-            configureButton();
-        }
-
         //https://stackoverflow.com/questions/33865445/gps-location-provider-requires-access-fine-location-permission-for-android-6-0/33866120
         //https://www.youtube.com/watch?v=ZgmeSK2GZDs
 
@@ -129,23 +83,42 @@ public class AbcParcelaFragment extends Fragment {
         return rootView;
     }
 
+    public void showToolbar(String tittle, boolean upButton){
+        Toolbar toolbar = getActivity().findViewById(R.id.toolbar_abc);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar();
+        ((AppCompatActivity)getActivity()).getSupportActionBar();
+
+    }
+
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode){
-            case 10:
-                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                    configureButton();
-                return;
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == 9) {
+
+            Toast.makeText(getActivity().getApplicationContext(), "accion bar", Toast.LENGTH_LONG).show();
         }
-    }
+        if (id == 9) {
 
-    private void configureButton() {
-        ubicacion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
-            }
-        });
-    }
+            // Do something
+            return true;
+        }
+        if (id == 0) {
 
+            // Do something
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
