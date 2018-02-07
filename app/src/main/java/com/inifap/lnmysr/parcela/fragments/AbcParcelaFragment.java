584081be99ce990aa386172009c17e380906d474
ghia115 <casaghia115@gmail.com>
2018-02-07 16:05:20 -0600

    location with gps

--- app/src/main/java/com/inifap/lnmysr/parcela/fragments/AbcParcelaFragment.java
+++ app/src/main/java/com/inifap/lnmysr/parcela/fragments/AbcParcelaFragment.java
@@ -1,11 +1,21 @@
 package com.inifap.lnmysr.parcela.fragments;
 
 
+import android.Manifest;
 import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.sqlite.SQLiteDatabase;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -18,6 +28,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 import android.support.v7.widget.Toolbar;
 
+import com.inifap.lnmysr.parcela.MainActivity;
 import com.inifap.lnmysr.parcela.R;
 import com.inifap.lnmysr.parcela.helperDataBase.BBDD_Helper;
 import com.inifap.lnmysr.parcela.helperDataBase.Estructura_BBDD;
@@ -28,8 +39,9 @@ import com.inifap.lnmysr.parcela.helperDataBase.Estructura_BBDD;
 public class AbcParcelaFragment extends Fragment {
 
     private EditText parcela, latitud, longitud;
-    FloatingActionButton ubicacion;
     Button updateBase;
+    LocationManager locationManager;
+    double longitudeGPS, latitudeGPS;
 
     public AbcParcelaFragment() {
         // Required empty public constructor
@@ -46,7 +58,6 @@ public class AbcParcelaFragment extends Fragment {
         parcela = rootView.findViewById(R.id.parcela);
         latitud = rootView.findViewById(R.id.latitud);
         longitud = rootView.findViewById(R.id.longitud);
-        ubicacion = rootView.findViewById(R.id.ubicacion);
         updateBase = rootView.findViewById(R.id.updateBase);
 
         updateBase.setOnClickListener(new View.OnClickListener() {
@@ -82,6 +93,81 @@ public class AbcParcelaFragment extends Fragment {
 
         return rootView;
     }
+
+
+    //https://medium.com/@victor.garibayy/obteniendo-mi-ubicaci%C3%B3n-en-android-studio-377226910823
+    private boolean checkLocation() {
+        if (!isLocationEnabled())
+            showAlert();
+        return isLocationEnabled();
+    }
+
+    private void showAlert() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(getContext());
+        dialog.setTitle("Enable Location")
+                .setMessage("Su ubicación esta desactivada.\npor favor active su ubicación " +
+                        "usa esta app")
+                .setPositiveButton("Configuración de ubicación", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(myIntent);
+                    }
+                })
+                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    }
+                });
+        dialog.show();
+    }
+
+    public void toggleGPSUpdates(View view) {
+        if (!checkLocation())
+            return;
+        Button button = (Button) view;
+        if (button.getText().equals(getResources().getString(R.string.pause))) {
+            locationManager.removeUpdates(locationListenerGPS);
+            button.setText(R.string.resume);
+        } else {
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            }
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER, 2 * 20 * 1000, 10, locationListenerGPS);
+            button.setText(R.string.pause);
+        }
+    }
+
+    private boolean isLocationEnabled() {
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    private final LocationListener locationListenerGPS = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            longitudeGPS = location.getLongitude();
+            latitudeGPS = location.getLatitude();
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    longitud.setText(longitudeGPS + "");
+                    latitud.setText(latitudeGPS + "");
+                    Toast.makeText(getContext(), "GPS Provider update", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+        }
+        @Override
+        public void onProviderDisabled(String s) {
+        }
+    };
 
     public void showToolbar(String tittle, boolean upButton){
         Toolbar toolbar = getActivity().findViewById(R.id.toolbar_abc);
